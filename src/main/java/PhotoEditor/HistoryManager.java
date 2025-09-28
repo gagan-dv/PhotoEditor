b6204aaffdab73898f3e77bda136979e083caf1e
Gagan Dev P V <gagandevstm123@gmail.com>
2025-09-27 22:02:26 -0700

    Update HistoryManager.java

--- src/main/java/PhotoEditor/HistoryManager.java
+++ src/main/java/PhotoEditor/HistoryManager.java
@@ -2,33 +2,36 @@ package PhotoEditor;
 
 import java.awt.image.BufferedImage;
 import java.util.Stack;
+
 class HistoryManager {
     private final Stack<BufferedImage> undoStack = new Stack<>();
     private final Stack<BufferedImage> redoStack = new Stack<>();
+
     void push(BufferedImage img) {
         if (img != null) {
             undoStack.push(Utils.deepCopy(img));
             redoStack.clear();
         }
     }
+
     void undo(CanvasPanel canvas) {
         if (!undoStack.isEmpty() && canvas.getImage() != null) {
             redoStack.push(Utils.deepCopy(canvas.getImage()));
-            BufferedImage prev = undoStack.pop();
-            canvas.setImage(Utils.deepCopy(prev));
+            canvas.setImage(Utils.deepCopy(undoStack.pop()));
         }
     }
+
     void redo(CanvasPanel canvas) {
         if (!redoStack.isEmpty() && canvas.getImage() != null) {
             undoStack.push(Utils.deepCopy(canvas.getImage()));
-            BufferedImage next = redoStack.pop();
-            canvas.setImage(Utils.deepCopy(next));
+            canvas.setImage(Utils.deepCopy(redoStack.pop()));
         }
     }
+
     BufferedImage resetToFirst() {
-        if (undoStack.isEmpty()) return null;
-        return Utils.deepCopy(undoStack.firstElement());
+        return undoStack.isEmpty() ? null : Utils.deepCopy(undoStack.firstElement());
     }
+
     void clear() {
         undoStack.clear();
         redoStack.clear();
